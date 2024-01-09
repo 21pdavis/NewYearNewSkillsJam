ee92f92af9b736e8f3cb7f198957696482099a53
Paul Davis <paulcdavis135@gmail.com>
2024-01-09 01:56:59 -0500

    Working basic vine generation!

--- Scenes/Presets/vine.gd
+++ Scenes/Presets/vine.gd
@@ -1,17 +1,65 @@
 extends Node2D
 
 var vine_segment := preload("res://Scenes/Presets/vine_segment.tscn")
+
+@onready var anchor: StaticBody2D = $Anchor
 var segments: Array[RigidBody2D]
 var joints: Array[PinJoint2D]
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func _process(_delta):
+	queue_redraw()
 
-func draw_vine(end):
-	var start := position
-	# TODO: add vines here
+# TODO: dynamic vine lengths (another parameter? Dynamic detection based on segment height?)
+func draw_vine(attach_point: Node2D):
+	var start := global_position
+	# start and end are in global
+	var spawn_direction := to_local((attach_point.global_position - start).normalized())
+	# determine rough number of vines (can tweak this later, make it dynamic, etc.), for now just static
+	var vine_num = 3
+	
+	# create and position vines
+	# TODO: off by a pixel here, should add 1 in some places
+	var segment_multiplier := 1.0/2
+	for i in range(vine_num):
+		# instantiate new vine segment
+		var current_segment := vine_segment.instantiate() as RigidBody2D
+		current_segment.name = "Segment{}".format([i + 1], "{}")
+		add_child(current_segment)
+		segments.append(current_segment)
+		
+		# position and rotate segment
+		# TODO: rotation
+		var segment_sprite := current_segment.get_node("Sprite") as Sprite2D
+		var sprite_height := segment_sprite.texture.get_height()
+		current_segment.position = segment_multiplier * sprite_height * (start + spawn_direction)
+		
+		# 1/2 --> 3/2 --> etc. (pivot at center of segment)
+		segment_multiplier += 1
+	
+	# create and position joints, connect segments
+	# TODO: messy, should use a diff method
+	var segment_height = (segments[0].get_node("Sprite") as Sprite2D).texture.get_height()
+	var joint_multiplier = 0
+	for i in range(vine_num + 1):
+		var current_joint := PinJoint2D.new()
+		current_joint.name = "Joint{}".format([i + 1], "{}")
+		add_child(current_joint)
+		joints.append(current_joint)
+		
+		current_joint.position = joint_multiplier * segment_height * (start + spawn_direction)
+		
+		# anchor to first segment connection
+		if i == 0:
+			current_joint.node_a = anchor.get_path()
+			current_joint.node_b = segments[0].get_path()
+		# last segment to attach_point connection
+		elif i == vine_num:
+			current_joint.node_a = segments[-1].get_path()
+			current_joint.node_b = attach_point.get_parent().get_path()
+		# segment-to-segment connections
+		else:
+			current_joint.node_a = segments[i].get_path()
+			current_joint.node_b = segments[i - 1].get_path()
+		
+		joint_multiplier += 1
--- Scenes/plant_mass.gd
+++ Scenes/plant_mass.gd
@@ -5,30 +5,36 @@ signal init_vine
 @export var segments: int
 
 # TODO: multiple connection points
-@onready var vine_connection_point: Node2D = $VinePoint
+#@onready var vine_connection_point: Node2D = $VinePoint
+@onready var vine_raycast: RayCast2D = $VineRaycast
 var vine_ref := preload("res://Scenes/Presets/vine.tscn")
 var vine_instance: Node2D
-var spawned_vine: bool
+var vine_spawned: bool
+
+# DEBUG
+var debug_intersection_point := Vector2.ZERO
 
 func _ready():
-	pass
-
-func _physics_process(delta):
-	# find spawn point of vine
-	if not spawned_vine:
-		var space_state := get_world_2d().direct_space_state
-		var query := PhysicsRayQueryParameters2D.create(vine_connection_point.position, vine_connection_point.position - 1e6 * transform.y)
-		var result := space_state.intersect_ray(query)
-
-		# build vine
-		if result:
-			vine_instance = vine_ref.instantiate()
-			vine_instance.position = result.position
-			vine_instance.draw_vine(vine_connection_point.position)
-			spawned_vine = true
-
-func _draw():
-	draw_line(vine_connection_point.position, vine_connection_point.position - 1e6 * transform.y, Color.GREEN, 1)
+	assert(vine_ref, "Vine scene not loaded correctly.")
 
 func _process(delta):
-	pass
+	queue_redraw()
+
+# TODO: refactor with Raycast2D node
+func _physics_process(delta):
+	if vine_raycast.is_colliding() and not vine_spawned:
+		var intersection_point := vine_raycast.get_collision_point()
+		
+		# DEBUG
+		debug_intersection_point = intersection_point
+		
+		vine_instance = vine_ref.instantiate() as Node2D
+		get_tree().current_scene.add_child(vine_instance)
+		vine_instance.position = intersection_point
+		# TODO: global?
+		vine_instance.draw_vine(vine_raycast)
+		vine_spawned = true;
+		
+#func _draw():
+	#if debug_intersection_point != Vector2.ZERO:
+		#draw_circle(to_local(debug_intersection_point), 2, Color.GREEN)
