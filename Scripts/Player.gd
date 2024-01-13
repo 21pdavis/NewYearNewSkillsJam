112597568e5e64622e853168d63372b9368bbf25
Michael Ross <mross3395@gmail.com>
2024-01-13 12:06:48 -0500

    im commiting

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -10,6 +10,8 @@ var state_machine
 var prev_velocity
 var myMarker : Marker2D
 var myMarker2 : Marker2D
+var shootTimer : Timer
+var isShooting : bool
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -19,6 +21,8 @@ func _ready():
 	state_machine = $AnimationTree.get("parameters/playback")
 	myMarker = $CollisionShape2D2/Marker2D
 	myMarker2 = $CollisionShape2D2/Marker2D2
+	shootTimer = $Timer
+	isShooting = false
 	
 
 func _physics_process(delta):
@@ -49,24 +53,34 @@ func _physics_process(delta):
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		if is_on_floor():	
-			state_machine.travel("IDLE")
+			if isShooting == true:
+				state_machine.travel("SHOOT_IDLE")
+			else:
+				state_machine.travel("IDLE")
 		
 	if look_direction == 1:
 		sprite.flip_h = false
 	else:
 		sprite.flip_h = true
 		
-	if Input.is_action_just_pressed("ui_select"):
+	if Input.is_action_pressed("ui_select") and shootTimer.is_stopped():
 		shoot()
+	else:
+		isShooting = false
 
 	move_and_slide()
+	
+#func _process(delta):
+	
 
 
 func shoot():
 	var bullet = bulletPath.instantiate()
 	bullet.direction(not sprite.flip_h)
 	get_parent().add_child(bullet)
+	isShooting = true
 	if sprite.flip_h == false:
 		bullet.position = myMarker.global_position
 	else:
 		bullet.position = myMarker2.global_position
+	shootTimer.start()
